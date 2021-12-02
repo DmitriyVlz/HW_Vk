d1f5fb9383c09521ee2bbf46ba0b86b5dcd89328
DmitriyVlz <appattit@mail.ru>
2021-12-02 18:25:49 +0300

    ferst commit

---
+++ HW_VK/src/main/java/ru/netology/domain/GeoLocation.java
@@ -0,0 +1,32 @@
+package ru.netology.domain;
+
+public class GeoLocation {
+    private String type;  // location type
+    private String coordinates;  //  location coordinates
+    private Place place;  // place description
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+}
+
